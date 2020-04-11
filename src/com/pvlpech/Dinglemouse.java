a103e77c99feacb7aba9236b3c07180705c3b149
PvlPech <pvlpech@gmail.com>
2020-04-11 23:09:40 +0400

    Dinglemouse keyboard constant initialization

--- src/com/pvlpech/Dinglemouse.java
+++ src/com/pvlpech/Dinglemouse.java
@@ -4,8 +4,21 @@ package com.pvlpech;
 
 public class Dinglemouse {
 
+    private static String[][] keyboard = new String[][]{
+            {"a", "b", "c", "d", "e", "1", "2", "3"},
+            {"f", "g", "h", "i", "j", "4", "5", "6"},
+            {"k", "l", "m", "n", "o", "7", "8", "9"},
+            {"p", "q", "r", "s", "t", ".", "@", "0"},
+            {"u", "v", "w", "x", "y", "z", "_", "/"}
+    };
+
     public static int tvRemote(final String word) {
-        // Your code here
+//        for(int i = 0; i < keyboard.length; i++){
+//            for(int j = 0; j < keyboard[0].length; j++){
+//                System.out.print(keyboard[i][j]);
+//            }
+//            System.out.print("\n");
+//        }
         return 1;
     }
 
