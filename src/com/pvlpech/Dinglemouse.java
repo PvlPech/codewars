a755864d0e0358e1b31ac2a833856bda1e8f111f
PvlPech <pvlpech@gmail.com>
2020-04-11 22:55:16 +0400

    Dinglemouse init commit

---
+++ src/com/pvlpech/Dinglemouse.java
@@ -0,0 +1,12 @@
+package com.pvlpech;
+
+/* TV Remote */
+
+public class Dinglemouse {
+
+    public static int tvRemote(final String word) {
+        // Your code here
+        return 1;
+    }
+
+}
--- src/com/pvlpech/Main.java
+++ src/com/pvlpech/Main.java
@@ -7,8 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 //        System.out.println(NumberZooPatrol.findMissingNumber(new int[]{1, 2}));
-        System.out.println(BankAccount.parse(" _  _  _  _  _  _  _ \n"+
-                                                     "|_| _| _||_||_ |_ |_|\n"+
-                                                     "|_||_  _||_| _||_| _|\n"));
+
+//        System.out.println(BankAccount.parse(" _  _  _  _  _  _  _ \n"+
+//                                                     "|_| _| _||_||_ |_ |_|\n"+
+//                                                     "|_||_  _||_| _||_| _|\n"));
+
+        System.out.println(Dinglemouse.tvRemote("olololo"));
     }
 }
